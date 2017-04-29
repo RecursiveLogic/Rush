fb5cd5a7f4b2f878f298d3bce2ef6bd236bcfe8c
RecursiveLogic <luis@radiux.io>
2017-04-28 23:27:41 -0400

    Updated regex

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -6,7 +6,7 @@ struct Parser;
 
 impl Parser {
     fn grab_tokens(&self, input: &str) -> Vec<&str> {
-        let tokens = Regex::new(r"[|&<>;$]+").unwrap();
+        let tokens = Regex::new(r"-?[a-zA-Z]+|[.<>|&$]+").unwrap();
         let matches: Vec<&str> = tokens
             .find_iter(input)
             .map(|x| x.as_str())
